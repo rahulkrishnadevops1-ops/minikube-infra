29503bff889c98ee9b625365b1e43199629ab023
Narendra-Geddam <don8197257610@gmail.com>
2026-03-12 13:31:45 +0530

    added port 8443 to access API server

--- infra/main.tf
+++ infra/main.tf
@@ -73,6 +73,14 @@ resource "aws_security_group" "minikube" {
   }
 
   ingress {
+    description = "Kubernetes API Server"
+    from_port   = 8443
+    to_port     = 8443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "NodePort services"
     from_port   = 30000
     to_port     = 32767
